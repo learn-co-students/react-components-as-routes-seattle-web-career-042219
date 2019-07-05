a20062924becec8bab7cd4b0c1032d9e0bf69735
Danny Tseng <dtfocus1025@gmail.com>
2019-07-05 13:01:13 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import Navbar from './Navbar';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+
+
+
+
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </React.Fragment>
+    
+  </Router>),
   document.getElementById('root')
 );
